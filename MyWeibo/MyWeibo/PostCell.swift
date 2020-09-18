1af0653ebe8cc64e1ead88e799e3798da628ca56
Glorious <Glorious>
2020-09-18 18:50:19 +0800

    Add new avatar jpg

--- MyWeibo/MyWeibo/PostCell.swift
+++ MyWeibo/MyWeibo/PostCell.swift
@@ -4,62 +4,110 @@
 //
 //  Created by BaronZhang on 2020/9/17.
 //
-
+//程序主界面
 import SwiftUI
 
 struct PostCell: View {
+    let post: Post
     var body: some View {
-        //水平排列
-        HStack(spacing: 5) {
-            //显示头像
-            Image(uiImage: UIImage(named: "630584a6gy1gau54zl806j20m80etmzo.jpg")!)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 50, height: 50)
-                .clipShape(Circle())
-                //添加Vip标示
-                .overlay(
-                    PostVIPBadge()
-                        .offset(x: 16, y: 16)
-                )
-                
-            
-            
-            //纵向排列用户昵称和时间
-            VStack(alignment: .leading, spacing: 5) {
-                Text("用户昵称")
-                    .font(Font.system(size: 16))
-                    .foregroundColor(Color(red: 242 / 255, green: 99 / 255, blue: 4 / 255))
-                    .lineLimit(1)
-                Text("2020-09-11")
-                    .font(.system(size: 11))
-                    .foregroundColor(.gray)
-//                Image(uiImage: UIImage(named: "avatar.jpg")!)
-                
-            }
-            .padding(.leading, 10) //调整上下左右的间距
-            //空间占位使昵称和关注按钮分别在屏幕两侧
-            Spacer()
-            
-            Button(action: {
-                print("Click Following Button")
-            }) {
-                Text("关注")
-                    .font(.system(size: 14))
-                    .foregroundColor(.orange)
-                    .frame(width: 50, height: 26) //设置按钮的宽高，方便设置圆角半径，并增大按钮点击区域
-                    .overlay( //叠加视图画圆角矩形
-                        RoundedRectangle(cornerRadius: 13)
-                            .stroke(Color.orange, lineWidth: 1) //描边
+        //增加微博详情+图片
+        VStack(alignment: .leading, spacing: 10) {
+            //水平头像/昵称/关注按钮
+            HStack(spacing: 5) {
+                //显示头像
+                post.avatarImage
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                    //添加Vip标示
+                    .overlay(
+                        PostVIPBadge(vip: post.vip)
+                            .offset(x: 16, y: 16)
                     )
+                //纵向排列用户昵称和时间
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(post.name)
+                        .font(Font.system(size: 16))
+                        .foregroundColor(Color(red: 242 / 255, green: 99 / 255, blue: 4 / 255))
+                        .lineLimit(1)
+                    Text(post.date)
+                        .font(.system(size: 11))
+                        .foregroundColor(.gray)
+                    
+                }
+                .padding(.leading, 10) //调整上下左右的间距
+                //关注按钮
+                if !post.isFollowed {
+                    //空间占位使昵称和关注按钮分别在屏幕两侧
+                    Spacer()
+                    
+                    Button(action: {
+                        print("Click Following Button")
+                    }) {
+                        Text("关注")
+                            .font(.system(size: 14))
+                            .foregroundColor(.orange)
+                            .frame(width: 50, height: 26) //设置按钮的宽高，方便设置圆角半径，并增大按钮点击区域
+                            .overlay( //叠加视图画圆角矩形
+                                RoundedRectangle(cornerRadius: 13)
+                                    .stroke(Color.orange, lineWidth: 1) //描边
+                            )
+                    }
+                    .buttonStyle(BorderlessButtonStyle())
+                    
+                }
             }
-
+            //微博内容
+            Text(post.text)
+                .font(.system(size: 17)) //字体大小
+            if !post.images.isEmpty {
+//                Image(uiImage: UIImage(named: post.images[0]))
+                loadImage(name: post.images[0])
+                    .resizable()
+                    .scaledToFill() //保持宽高比填充整个空间
+                    .frame(width: UIScreen.main.bounds.width - 30, height: (UIScreen.main.bounds.width - 30) * 0.75) //设置显示宽高比4:3，width：屏幕宽度 - 左右两边的间距（30） height: 宽度 * 0.75
+                    .clipped()
+            }
+            //增加分割线
+            Divider()
+            //添加评论+点赞按钮
+            HStack(spacing: 0) {
+                Spacer()
+                
+                PostCellToBarButton(image: "message",
+                                    text: post.commentCountText,
+                                    color: .black)
+                {
+                    print("Click Comment Button")
+                }
+                
+                Spacer()
+                
+                PostCellToBarButton(image: "heart",
+                                    text: post.likeCountText,
+                                    color: .black)
+                {
+                    print("Click Like Button")
+                }
+                Spacer()
+            }
+            //添加一个与屏幕两边都连接的矩形（分割线）
+            Rectangle()
+                .padding(.horizontal, -15)
+                .frame(height: 10)
+                .foregroundColor(Color(red: 238 / 255, green: 238 / 255, blue: 238 / 255))
         }
+        .padding(.horizontal, 15) //设置VStack和屏幕两边的间距为15
+        .padding(.top, 15) //添加上部间距
+        
+
     }
 }
 
 struct PostCell_Previews: PreviewProvider {
     static var previews: some View {
-        PostCell()
+        PostCell(post: postList.list[7])
+        
     }
 }
